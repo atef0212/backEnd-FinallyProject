1abc0d71ba75f1d6868dc9178e3638f74fe001d3
atef0212 <126253950+atef0212@users.noreply.github.com>
2024-03-28 15:28:04 +0100

    folders added

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-import express from 'express'
+import express from "express";
 
-import { connectToDatabase } from './db-connection.js'
-const app=express()
-const port=4000
-connectToDatabase()
-app.listen(port, ()=>{
-    console.log(`Server running on port ${port}`)
-})
+import { connectToDatabase } from "./db-connection.js";
+const app = express();
+const port = 4000;
+connectToDatabase();
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
