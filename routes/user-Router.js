2004edc32fb9c88dbc6d92a7780401286bbeb018
atef0212 <126253950+atef0212@users.noreply.github.com>
2024-03-29 11:37:36 +0100

    update

--- routes/user-Router.js
+++ routes/user-Router.js
@@ -3,7 +3,7 @@ import { useValidator, validate } from '../maddleWare/useValidator.js'
 import { getUser, addUser } from '../controllers/userController.js'
  const useRouter=express.Router()
  useRouter.get("/", getUser)
- useRouter.post("/",useValidator, validate,   addUser)
+ useRouter.post("/signup",useValidator, validate,   addUser)
 
  export default useRouter
 
