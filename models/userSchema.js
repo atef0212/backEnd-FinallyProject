97817eed25923796c599848169744b631af8d31c
atef0212 <126253950+atef0212@users.noreply.github.com>
2024-04-02 10:49:18 +0200

    updated

--- models/userSchema.js
+++ models/userSchema.js
@@ -2,11 +2,11 @@ import mongoose from "mongoose"
 const genders = ['male', 'female', 'other'];
 const userSch= new mongoose.Schema({
     name: { type: String, required: true },
-    gender: {
-      type: String,
-      enum: genders,
-      required: true,
-    },
+    // gender: {
+    //   type: String,
+    //   enum: genders,
+    //   required: true,
+    // },
     email: { type: String, required: true, unique: true },
   password: { type: String, required: true, minlength: 6 },
   //  image: { type: String, required: true },
--- routes/user-Router.js
+++ routes/user-Router.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import { useValidator, validate } from '../maddleWare/useValidator.js'
-import { getUser, signUp } from '../controllers/userController.js'
+import { getUser, signUp, logIn } from '../controllers/userController.js'
  const useRouter=express.Router()
  useRouter.get("/", getUser)
  useRouter.post("/signup",useValidator, validate,   signUp)
+ useRouter.post("/logIn",useValidator, validate,   logIn)
 
  export default useRouter
 
