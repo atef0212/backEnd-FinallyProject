47a8ed4dc141e946cf22f725f0b51bf2c2b15200
atef0212 <126253950+atef0212@users.noreply.github.com>
2024-04-05 08:45:40 +0200

    installed cors

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 import express from "express";
+import bodyParser from "body-parser"
 import useRouter from "./routes/user-Router.js";
 import contenRoutes from "./routes/contect-routes.js";
 import { connectToDatabase } from "./db-connection.js";
+import cors from 'cors'
 const app = express();
 const port = 4000;
 
 app.use(express.json())
+app.use(bodyParser.json())
+app.use(cors())
+app.use((req, res, next)=>{
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Accept, Authorization")
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE")
+  next()
+})
 connectToDatabase();
 
 app.use("/api/content", contenRoutes)
